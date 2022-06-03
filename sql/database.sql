a3af75924ccffdb5e00a0643e86291999f6d3946
Marcel <61519464+falscherIdiot@users.noreply.github.com>
2022-06-03 09:53:47 +0000

    fixed bugs

--- sql/database.sql
+++ sql/database.sql
@@ -1,40 +1,40 @@
 /*
 USER Table
 */
-DROP TABLE IF EXISTS 'users';
-CREATE TABLE 'users' (
-    'id' INTEGER PRIMARY KEY AUTO_INCREMENT,
-    'username' TEXT NOT NULL,
-    'password' TEXT NOT NULL
+DROP TABLE IF EXISTS users;
+CREATE TABLE users (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(30) NOT NULL,
+    password VARCHAR(100) NOT NULL
 );
-INSERT INTO 'users' ('username', 'password') VALUES
+INSERT INTO users (username, password) VALUES
 ('admin', '370270537ce75eab5cdb087cb2a6dcc25f18bcf540ebe23868e9be43c5e4f47'),
 ('user', '3ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4');
 
 /*
 Default LUNA events [CORE-ONLY]
 */
-DROP TABLE IF EXISTS 'registered_events';
-CREATE TABLE 'registered_events' (
-    'name' TEXT PRIMARY KEY NOT NULL,
-    'description' TEXT NOT NULL
+DROP TABLE IF EXISTS registered_events;
+CREATE TABLE registered_events (
+    name VARCHAR(30) PRIMARY KEY NOT NULL,
+    description VARCHAR(30) NOT NULL
 );
-INSERT INTO 'registered_events' ('name', 'description') VALUES
+INSERT INTO registered_events (name, description) VALUES
 ('core::stop', 'Shutdowns LUNA'),
 ('core::help', 'Shows this Menu');
 
 /*
 TODO Table
 */
-DROP TABLE IF EXISTS 'todo';
-CREATE TABLE 'todo' (
-  'id' INTEGER PRIMARY KEY AUTO_INCREMENT,
-  'title' TEXT NOT NULL,
-  'description' TEXT NOT NULL,
-  'created_at' DATE DEFAULT CURRENT_TIMESTAMP,
-  'created_by' INTEGER NOT NULL
+DROP TABLE IF EXISTS todo;
+CREATE TABLE todo (
+  id INTEGER PRIMARY KEY AUTO_INCREMENT,
+  title VARCHAR(30) NOT NULL,
+  description VARCHAR(100) NOT NULL,
+  created_at DATE DEFAULT CURRENT_TIMESTAMP,
+  created_by INTEGER NOT NULL
 );
-INSERT INTO 'todo' ('title', 'description', 'created_by') VALUES
+INSERT INTO todo (title, description, created_by) VALUES
 ('Todo 1', 'Description 1', 0),
 ('Todo 2', 'Description 2', 0),
 ('Todo 3', 'Description 3', 0),
